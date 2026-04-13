f390a68131e226b8306a51722152b6622ad00768
user0guest590 <samkelmaneevon@gmail.com>
2026-04-13 07:39:06 -0700

    Update license comment format in appcsc.js

--- appcsc.js
+++ appcsc.js
@@ -1,6 +1,6 @@
-/*!
- * MIT License
- * * Copyright (c) 2026 APPCSC (Application Codin Script)
+/**!
+ * @license MIT
+ * * Copyright (c) 2026 APPCSC (Application Coding Script)
  * * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
